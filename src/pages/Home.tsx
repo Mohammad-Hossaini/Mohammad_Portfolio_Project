91b1ffcee21f9821bec75466dc4bfe5f790cc774
Noor Mohammad Sirath <noormohammadsirath5@gmail.com>
2025-12-14 06:06:31 -0800

    First commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import ThemToggle from "../components/ThemToggle";
+
+function Home() {
+  return (
+    <div className="min-h-screen text-center bg-background text-foreground overflow-x-hidden ">
+      {/* Them Toggle */}
+      <ThemToggle />
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default Home;
